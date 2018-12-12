046327cf9ea53821a5972ec84f906d1565e40be6
vikassumani <vikassumani@gmail.com>
2018-12-12 20:19:03 +0530

    commited mavenjenkins project

---
+++ src/test/java/jenkinsMavenPackage/JenkinsMavenClassTest.java
@@ -0,0 +1,16 @@
+package jenkinsMavenPackage;
+
+import org.testng.annotations.Test;
+
+public class JenkinsMavenClassTest {
+
+//	public static void main(String[] args) {
+//		System.out.println("This is Jenkins Maven GIT Integration Project");
+//
+//	}
+	@Test
+	public void test() {
+		System.out.println("This is Jenkins Maven GIT Integration Project");
+	}
+
+}
